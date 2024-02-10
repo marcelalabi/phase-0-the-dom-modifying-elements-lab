7c23125e08065a4e87c49e8983418290e7a509d5
Malik Alabi <marcelalabi776@gmail.com>
2024-02-10 13:22:35 -0600

    Done

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+const mainNode = document.querySelector('main');
+mainNode.remove();
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'MARCEL is the champion';
